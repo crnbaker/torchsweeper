57aad977c914ae64ae80536a03779038746ef2b0
Christian Baker <31904251+crnbaker@users.noreply.github.com>
2021-01-08 17:03:46 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-       'numpy',
-       'pytorch'
+       'numpy'
     ]
     python_requires='>=3.6'
 )
