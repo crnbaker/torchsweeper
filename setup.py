79738a621bf580626b2ab6b6f4132c8368138523
Christian Baker <31904251+crnbaker@users.noreply.github.com>
2021-01-08 16:26:19 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchsweeper-crnbaker",
+    version="0.0.1",
+    author="Christian Baker",
+    author_email="christian.baker@kcl.ac.uk",
+    description="Classes for timing and parameter sweeping PyTorch and SciPy code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crnbaker/torchsweeper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
